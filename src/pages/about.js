0aa5993842da89afb8cfe0bed674d5366234a3d4
nishimura <saishuheki.nisshing@gmail.com>
2022-04-04 17:43:57 +0900

    GraphQLからQiitaRSSを表示

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const AboutPage = () => {
+    return <p>My Profile</p>
+}
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,53 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 
 // Gatsby リンクコンポーネントの呼び出し
 import { Link } from "gatsby";
 
-const IndexPage = () => {
+
+
+const PostItem = (props) => {
+  const {title, link, pubDate } = props.post;
   return (
-    <div>
-      <p>hello Gatsby</p>
+    <li>
+      <a href={link}>
+        <small>{pubDate}</small> {title}
+      </a>
+    </li>
+  )
+};
+
+
+
+// props.dataにquery内に記載したGraphQLの結果が入る
+const IndexPage = (props) => {
+  return (
+    <ul>
+      {props.data.allFeedQiita.nodes.map((post) => {
+        return <PostItem post={post} key={post.link} />
+      })}
 
       {/* aboutページへのリンク */}
-      <Link to="/about">Link to about</Link>
+      {/* <Link to="/about">Link to about</Link> */}
 
-    </div>
-  )
-}
+      {/* GraphQL データの表示 */}
+      {/* <pre>{JSON.stringify(props.data, null, 2)}</pre> */}
+
+    </ul>
+  );
+};
+
+// qiitaのデータを表示
+export const query = graphql`
+  query MyQuery {
+    allFeedQiita {
+      nodes {
+        title
+        link
+        pubDate
+      }
+    }
+  }
+`;
 
 export default IndexPage;
